f5982db54145da92021bc9f8a31fabd6a3ad8b82
Duarte Carrilho da Graça <duarte.carrilho.freelance@theguardian.com>
2016-09-14 11:48:18 +0100

    adds page view tracking to embeds

--- Gruntfile.js
+++ Gruntfile.js
@@ -182,13 +182,13 @@ module.exports = function(grunt) {
             var localDir = path.split('/')[1];
             var project = grunt.file.readJSON(jsonFile);
             var embed = path + '/embed.html';
-            var documentBody = '<!DOCTYPE html><html><head><meta charset="UTF-8"><style>' + css + '</style><script src="//j.ophan.co.uk/interactive.js"></script></head><body>';
+            var documentBody = '<!DOCTYPE html><html><head><meta charset="UTF-8"><style>' + css + '</style></head><body>';
             if (grunt.file.exists(path + '/hashmap.json')) {
                 var hashedMap = grunt.file.readJSON(path + '/hashmap.json');
             }
 
             project['html'] = '<div class="' + localDir + '__wrapper">' + '<style>' + css + '</style>' + html + '</div>';
-            var embedHtml = documentBody + html + '</body></html>';
+            var embedHtml = documentBody + html + '<script src="//j.ophan.co.uk/interactive.js"></script></body></html>';
             grunt.file.expand({}, dir + '/_source/*').forEach(function(file) {
                 file = file.split("/");
                 file = file[file.length-1];
